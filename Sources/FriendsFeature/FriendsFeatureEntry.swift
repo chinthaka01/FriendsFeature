2dc546371e7337ab14e3a4d098c6e5c9abe1511b
chinthaka01 <chinthaka01@gmail.com>
2026-01-05 13:12:23 -0500

    Inialize viewModel once.

--- Sources/FriendsFeature/FriendsFeatureEntry.swift
+++ Sources/FriendsFeature/FriendsFeatureEntry.swift
@@ -10,19 +10,22 @@ struct FriendsFeatureEntry: @MainActor MicroFeature {
     let selectedTabIcon: UIImage
 
     private let dependencies: FriendsDependencies
+    private let viewModel: FriendsViewModel
 
+    @MainActor
     init(dependencies: FriendsDependencies) {
         self.dependencies = dependencies
         self.tabIcon = UIImage(systemName: "person.3")!
         self.selectedTabIcon = UIImage(systemName: "person.3")!
+        
+        self.viewModel = FriendsViewModel(
+            api: dependencies.friendsAPI,
+            analytics: dependencies.analytics
+        )
     }
 
     @MainActor
     func makeRootView() -> AnyView {
-        let viewModel = FriendsViewModel(
-            api: dependencies.friendsAPI,
-            analytics: dependencies.analytics
-        )
         return AnyView(FriendsRootView(viewModel: viewModel))
     }
 }
